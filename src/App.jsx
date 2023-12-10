c187f3b334a7fe07c6dfdf5f21ed7257007299b8
wahidaliyar <aliyar.wahid21@gmail.com>
2023-12-10 13:34:21 +0430

    Build static app

--- src/App.jsx
+++ src/App.jsx
@@ -21,5 +21,78 @@ const initialFriends = [
 
 
 export default function App() {
-  return <div className="app">How are you?</div>
+  return <div className="app">
+    <div className="sidebar">
+      <FriendList />
+      <FormAddFriend />
+      <Button>Add friend</Button>
+    </div>
+    <FormSplitBill />
+  </div>
+}
+
+
+
+function FriendList() {
+  const friends = initialFriends;
+
+  return <ul>
+    {friends.map((friend) => (
+      <Friend key={friend.id} friend={friend} />
+    ))}
+  </ul>
+}
+
+function Friend({friend}) {
+  return <li>
+    <img src={friend.image} alt={friend.name} />
+    <h3>{friend.name}</h3>
+
+    {friend.balance < 0 && <p className="red">You owe {friend.name} {Math.abs(friend.balance)}$</p>}
+    {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+    {friend.balance > 0 && <p className="green">{friend.name} owes you {Math.abs(friend.balance)}$</p>}
+
+    <Button>Select</Button>
+  </li>
+}
+
+
+function Button({children}) {
+  return <button className="button">{children}</button>
+}
+
+function FormAddFriend() {
+  return <form className="form-add-friend">
+    <label>🧑‍🤝‍🧑 Friend name</label>
+    <input type="text" />
+
+    <label>🏞️ Image URL</label>
+    <input type="text" />
+
+    <Button>Add</Button>
+  </form>
+}
+
+
+function FormSplitBill() {
+  return <form className="form-split-bill">
+    <h2>Split a bill with XX</h2>
+
+    <label>💰 Bill value</label>
+    <input type="text" />
+
+    <label>🤵 Your expense</label>
+    <input type="text" />
+
+    <label>🧑‍🤝‍🧑 X's expense</label>
+    <input type="text" disabled />
+
+    <label>📄 Who is paying the bill?</label>
+    <select>
+      <option value="user">You</option>
+      <option value="friend">Friend</option>
+    </select>
+
+    <Button>Split bill</Button>
+  </form>
 }
